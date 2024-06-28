917de52fe4fde1e15178b5f861e22a79e8ebc81a
Masoom Ali <masoomali.peer@cigniti.com>
2024-06-28 17:33:01 +0530

    otp updated
    otp updated

--- src/queries/settingQueries.js
+++ src/queries/settingQueries.js
@@ -2,3 +2,5 @@ export const fetchEmailAndPhone =
   "SELECT EmailAddress, PhoneNumber FROM user WHERE UserId = ?";
 export const updateEmailAndPhone =
   "UPDATE user SET EmailAddress = IFNULL(?, EmailAddress), PhoneNumber = IFNULL(?, PhoneNumber) WHERE UserId = ?";
+
+export const verifyOTP = `SELECT * FROM user WHERE UserId = ? AND OTP = ?`;
